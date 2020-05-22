cadb4da484c45024afdf194b7cfe5bee8fe8e0ac
alikemal.ocalan <alikemal.ocalan@trendyol.com>
2020-05-23 00:42:02 +0300

    Use okhttp3.dnsoverhttps Remove unnecessary depends

--- src/main/kotlin/com/github/alikemalocalan/greentunnel4jvm/utils/DNSOverHttps.kt
+++ src/main/kotlin/com/github/alikemalocalan/greentunnel4jvm/utils/DNSOverHttps.kt
@@ -1,40 +1,58 @@
 package com.github.alikemalocalan.greentunnel4jvm.utils
 
-import com.google.gson.Gson
+import okhttp3.Cache
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import okhttp3.dnsoverhttps.DnsOverHttps
+import okhttp3.logging.HttpLoggingInterceptor
+import java.io.File
+import java.net.InetAddress
+import java.net.UnknownHostException
 import java.util.concurrent.TimeUnit.SECONDS
 
-data class Answer(val TTL: Long, val data: String)
-
-data class CloudFlareResponse(val Answer: Array<Answer>)
-
 object DNSOverHttps {
-    fun requestUrl(url: String): String = "https://ads-doh.securedns.eu/dns-query?name=$url&type=A"
+    var logging: HttpLoggingInterceptor = HttpLoggingInterceptor()
 
-    val client by lazy {
+    val cache = Cache(
+        directory = File(System.getProperty("java.io.tmpdir"), "http_cache"),
+        // $0.05 worth of phone storage in 2020
+        maxSize = 50L * 1024L * 1024L // 10 MiB
+    )
+
+    private val client by lazy {
+        logging.setLevel(HttpLoggingInterceptor.Level.NONE)
         OkHttpClient.Builder()
+            .cache(cache)
+            .addInterceptor(logging)
             .connectTimeout(60, SECONDS)
             .writeTimeout(60, SECONDS)
             .readTimeout(60, SECONDS)
             .build()
     }
 
-    fun lookUp(address: String): String {
-        val response = doGetHttpCall(requestUrl(address)).body!!.string()
-        return ipAdressfromJson(response)
+    private val dns by lazy {
+        buildDnsClient(client)
     }
 
-    fun ipAdressfromJson(jsonStr: String): String =
-        Gson().fromJson(jsonStr, CloudFlareResponse::class.java)
-            .Answer.first().data
+    fun lookUp(address: String): String =
+        dns.lookup(address).first().hostAddress
 
 
-    fun doGetHttpCall(url: String): Response {
-        val request = Request.Builder()
-            .url(url).method("GET", null)
+    private fun buildDnsClient(bootstrapClient: OkHttpClient): DnsOverHttps {
+        return DnsOverHttps.Builder().client(bootstrapClient)
+            .url("https://1.1.1.1/dns-query".toHttpUrl())
+            .bootstrapDnsHosts(getByIp("1.1.1.1"), getByIp("1.0.0.1"))
+            .includeIPv6(false)
+            .post(true)
             .build()
-        return client.newCall(request).execute()
+    }
+
+    private fun getByIp(host: String): InetAddress {
+        return try {
+            InetAddress.getByName(host)
+        } catch (e: UnknownHostException) {
+            // unlikely
+            throw RuntimeException(e)
+        }
     }
 }
